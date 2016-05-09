8b545ac3350d0b5a849eef6c1a142b1d399582fc
Alex A <Alex A>
2016-05-09 12:00:32 -0400

    added random layout

--- R_Scripts/findCorrelations.R
+++ R_Scripts/findCorrelations.R
@@ -5,25 +5,49 @@ setwd('C:/Users/Alex/Documents/EpiStroma/R_Scripts')
 args <- commandArgs(trailingOnly = TRUE)
 gene <- args[2]
 side <- args[3]
-pValue <- args[4]
+neighbour <- args[4]
+pValue <- args[5]
 
 corMatrix <- dget(paste('corMatrix.', pValue, ".R", sep=""))
 degrees <- dget(paste('degrees.', pValue, ".R", sep=""))
 
 #gene	
 #side
+hasSelfLoop <- FALSE
 gene <- as.character(gene)
 if (side == '-e') {
-	resultDegrees <- degrees$stromaDegree[names( which(corMatrix[gene, ] != 0))]
+	index <- which(corMatrix[gene, ] != 0)
+	if (neighbour != '1') {
+		if (length(which(names(index) == gene)) > 0) {
+			hasSelfLoop <- TRUE
+		} else {
+			hasSelfLoop <- FALSE
+		}
+
+		index <- index[which(names(index) != gene)]	
+	}
+	
+	resultDegrees <- degrees$stromaDegree[index]#degrees$stromaDegree[names( which(corMatrix[gene, ] != 0))]
 	#print(as.character(which(corMatrix[gene, ] != 0)))
-	resultWeights <- corMatrix[gene, names(which(corMatrix[gene, ] != 0))]
-	names(resultWeights) <- names( which(corMatrix[gene, ] != 0))
-	result <- list(weights = resultWeights, degrees = resultDegrees)
+	resultWeights <- corMatrix[gene, index]#corMatrix[gene, names(which(corMatrix[gene, ] != 0))]
+	names(resultWeights) <- names(index)
+	result <- list(weights = resultWeights, degrees = resultDegrees, hasSelfLoop = hasSelfLoop)
 	cat(format(serializeJSON(result)))
 } else {
-	resultDegrees <- degrees$epiDegree[names( which(corMatrix[, gene] != 0))]
-	resultWeights <- corMatrix[names(which(corMatrix[, gene] != 0)), gene]
-	names(resultWeights) <- names(which(corMatrix[, gene] != 0))
-	result <- list(weights = resultWeights, degrees = resultDegrees)
+	index <- which(corMatrix[, gene] != 0)
+	if (neighbour != '1') {
+		if (length(which(names(index) == gene)) > 0) {
+			hasSelfLoop <- TRUE
+		} else {
+			hasSelfLoop <- FALSE
+		}
+
+		index <- index[which(names(index) != gene)]	
+	}
+	
+	resultDegrees <- degrees$epiDegree[index]
+	resultWeights <- corMatrix[index, gene]
+	names(resultWeights) <- names(index)
+	result <- list(weights = resultWeights, degrees = resultDegrees, hasSelfLoop = hasSelfLoop)
 	cat(format(serializeJSON(result)))
 }
