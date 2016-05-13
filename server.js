89d1c788298fcd89cdc338feb4766da7f2bd53f2
Alex A <Alex A>
2016-05-13 08:52:26 -0400

    Created a script for getting the relevant genes as well as their first and second neighbours

--- server.js
+++ server.js
@@ -277,183 +277,50 @@ app.post('/new-neighbour-general', function(req, res) {
         });
 });
 
-app.post('/neighbour-general', function(req, res) {
-    var neighbour = req.body.neighbour;
-    var gene = req.body.gene;
-    var side = req.body.side.toUpperCase();
-    var degree = req.body.degree;
-    var originalElements = req.body.originalElements;
-    var pValue = req.body.pValue;
-    var requestedLayout = req.body.layout;
-    var first = req.body.first;
-    var second = req.body.second;
-    var exclude = 'NA';
+app.get('/submatrix', function(req, res) {
+    var genes = req.query.genes;
+    var pValue = req.query.pValue;
 
-    console.log(req.body);
+    var geneArgs = "";
 
-    if (first == second) {
-        neighbour = 2;
-    } else {
-        neighbour = 1;
+    for (var i = 0; i < genes.length; i++) {
+        geneArgs = geneArgs + " " + "\"" + genes[i] + "\"";
     }
 
-    if (second != null) {
-        exclude = first;
-    }
-
-    var neighbourSide = side == "-E" ? "-S" : "-E"
-    var child = exec(
-        "Rscript R_Scripts/findCorrelations.R --args " +
-        "\"" + gene.toUpperCase() +
-        "\"" + " " + "\"" + side + "\"" + " " + "\"" + neighbour + "\"" +
-        " " + "\"" +
-        exclude + "\"" + " " + "\"" +
-        pValue + "\"", {
+    var child = exec("Rscript R_Scripts/getRelevantSubmatrix.R --args" geneArgs + " " + "\"" + pValue + "\"", {
             maxBuffer: 1024 *
                 50000
         },
         function(error, stdout, stderr) {
-            var elements = [];
+            //console.log('stdout: ' + stdout);
             console.log('stderr: ' + stderr);
 
             if (error != null) {
                 console.log('error: ' + error);
             }
 
-            var parsedValue = JSON.parse(stdout);
-            console.log(parsedValue);
+            var parsed = getWeightsAndDegreesFromROutput(stdout);
+            var elements = createElements(parsed.epiDegrees, parsed.stromaDegrees,
+                parsed.weights,
+                parsed.geneNames);
 
-            var weights = parsedValue.value[0];
-            var degrees = parsedValue.value[1].value;
-            var hasSelfLoop = parsedValue.value[2].value;
-            var neighbourGeneNames = weights.attributes.names.value;
-            var dimension = neighbourGeneNames.length;
-            var neighbourGeneWeights = weights.value;
-            var neighboursGenes = [];
-            var resultWeights = [];
-            var parent = "";
+            var allInfo = {
+                elements: elements,
+                totalInteractions: parsed.totalInteractions,
+                minPositiveWeight: parsed.minPositiveWeight,
+                maxPositiveWeight: parsed.maxPositiveWeight,
+                minNegativeWeight: parsed.minNegativeWeight,
+                maxNegativeWeight: parsed.maxNegativeWeight
+            };
 
-            if (neighbour == 1) {
-                parent = side == "-E" ? "stroma" : "epi";
-            } else {
-                parent = side == "-E" ? "stromaRight" : "epiRight";
-            }
-
-            var neighbourBgColor = side == "-E" ? "red" : "blue";
-            var neighbourNodes = nodeUtils.createNodes(neighbourGeneNames,
-                parent,
-                neighbour == 1 ?
-                4 : 8, degrees);
-            var sourceNode = nodeUtils.createNodes([gene], side == "-E" ?
-                "epi" :
-                "stroma", 1, [
-                    degree
-                ]);
-            var sourceBgColor = side == "-E" ? "blue" : "red";
-
-            if (requestedLayout == 'hierarchical') {
-                neighbourNodes = nodeUtils.addPositionsToNodes(
-                    neighbourNodes,
-                    neighbour ==
-                    1 ? 400 : 800, 100, 0, 20);
-                neighbourNodes = nodeUtils.addStyleToNodes(neighbourNodes,
-                    10, 10,
-                    "left",
-                    "center", neighbourBgColor);
-
-
-                sourceNode = nodeUtils.addPositionsToNodes(sourceNode, 100,
-                    100, 0, 0);
-                sourceNode = nodeUtils.addStyleToNodes(sourceNode, 10, 10,
-                    "left",
-                    "center",
-                    sourceBgColor);
-            }
-
-            var edges = edgeUtils.createEdgesFromNode(sourceNode[0],
-                neighbourNodes,
-                neighbourGeneWeights,
-                hasSelfLoop);
-
-            if (second == null) {
-                elements = elements.concat(sourceNode);
-
-                if (requestedLayout == "hierarchical") {
-                    elements.push({
-                        data: {
-                            id: 'epi'
-                        }
-                    });
-                    elements.push({
-                        data: {
-                            id: 'stroma'
-                        }
-                    });
-                }
-            } else {
-                elements = elements.concat(originalElements);
-
-                if (neighbourNodes.length > 0) {
-                    if (requestedLayout == "hierarchical") {
-                        elements.push({
-                            data: {
-                                id: side == '-E' ? 'stromaRight' : 'epiRight'
-                            }
-                        });
-                    }
-                }
-
-                var opposite = side == "-E" ? "-S" : "-E";
-
-                if (first == second) {
-                    if (hasSelfLoop) {
-                        elements.push({
-                            data: {
-                                id: 'selfLoop' + gene,
-                                source: gene + side,
-                                target: gene + opposite
-                            }
-                        });
-                    }
-                }
-            }
-
-            elements = elements.concat(neighbourNodes);
-            elements = elements.concat(edges);
-
-            var firstEdgeSelector = side == '-S' ? 'EpiToStroma' :
-                'StromaToEpi';
-            firstEdgeSelector = firstEdgeSelector + gene;
-            var config = configUtils.createConfig();
-
-            if (first == second) {
-                configUtils.addStyleToConfig(config, {
-                    'selector': 'edge[id = "selfLoop' + gene + '"]',
-                    'style': styleUtils.selfLoopEdge
-                });
-                configUtils.addStyleToConfig(config, {
-                    'selector': 'edge[id = "' + firstEdgeSelector +
-                        '"]',
-                    'style': styleUtils.selfLoopEdge
-                });
-            }
-
-            configUtils.setConfigElements(config, elements);
-
-            var layout = null;
-            if (requestedLayout == "hierarchical") {
-                layout = configUtils.createPresetLayout();
-            } else if (requestedLayout == "concentric") {
-                layout = configUtils.createConcentricLayout(sourceNode[0].data
-                    .id);
-            }
-
-            configUtils.setConfigLayout(config, layout);
-            res.json({ config: config });
+            res.json() = allInfo;
         });
+
 });
 
-
+function buildStringArgs(script, args) {
+    var stringArgs = "";
+}
 /*  For now, xPattern and yPattern will simply be increments to add at every step of the loop
  */
 function createOverallConfig(originalElements, requestedLayout) {
