0d4720a727f4f963c52927bc5ee955d34d059077
Alex A <Alex A>
2016-05-30 17:27:09 -0400

    Readded the file uploading service

---
+++ app/js/directives/staticInclude.js
@@ -0,0 +1,20 @@
+var myModule = angular.module("myApp");
+myModule.directive('staticInclude', function($http, $templateCache, $compile) {
+	var directive = {};
+	
+	directive.link = function(scope, element, attrs) {
+		var templatePath = attrs.staticInclude;
+		directive.templateURL = templatePath;
+	}
+
+	return directive;
+
+
+    // return function(scope, element, attrs) {
+    //     var templatePath = attrs.staticInclude;
+    //     $http.get(templatePath, { cache: $templateCache }).success(function(response) {
+    //         var contents = element.html(response).contents();
+    //         $compile(contents)(scope);
+    //     });
+    // };
+});
---
+++ app/js/services/FileUploadService.js
@@ -0,0 +1,38 @@
+var myModule = angular.module("myApp");
+myModule.factory('FileUploadService', function($http, Upload) {
+    var service = {};
+
+    service.uploadFiles = function(file, errFiles, scope) {
+        scope.f = file;
+        scope.errFile = errFiles && errFiles[0];
+
+        if (file) {
+            var r = new FileReader();
+            r.onload = function(e) {
+                var contents = e.target.result;
+
+                file.data = r.result;
+                file.upload = Upload.upload({
+                    url: 'http://localhost:5000/uploadMatrix',
+                    data: { file: file, data: r.result }
+                });
+
+                file.upload.then(function(response) {
+                    $timeout(function() {
+                        file.result = response.data;
+                    });
+                }, function(response) {
+                    if (response.status > 0)
+                        scope.errorMsg = response.status + ': ' + response.data;
+                }, function(evt) {
+                    file.progress = Math.min(100, parseInt(100.0 *
+                        evt.loaded / evt.total));
+                });
+            }
+
+            r.readAsDataURL(file);
+        }
+    }
+
+    return service;
+});
