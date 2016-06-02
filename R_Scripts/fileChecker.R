1ee05661de30205f8df43ecc8f614989312aea0b
Alex A <Alex A>
2016-06-02 09:43:32 -0400

    Made some changes to the way files are being retrieved

--- R_Scripts/fileChecker.R
+++ R_Scripts/fileChecker.R
@@ -13,12 +13,19 @@ write(fileName, stderr())
 corMatrix <- c()
 result <- list(returnCode = c(1,2), message = c(3,4))
 tryCatch(corMatrix <- readRDS(fileName),
-           error = function(cond) {cat(format(serializeJSON(cond))); file.remove(fileName); quit()}) 
+           error = function(cond) {cat(format(serializeJSON(cond))) ; write("failed read", stderr()); file.remove(fileName); quit()}) 
 
 rowNames <- rownames(corMatrix)
 colNames <- colnames(corMatrix)
 
+write('passed rowNames', stderr())
+write('rowNames', stderr())
+write(rowNames, stderr())
+write('colNames', stderr())
+write(colNames, stderr())
+
 if (!is.null(rowNames) && !is.null(colNames) && all(rowNames == colNames)) {
+	write('if:', stderr())
 	cat(format(serializeJSON(list(returnCode = 0, message = "Good to go"))))	
 	corMatrix <- appendSideToMatrixNames(corMatrix, 'E', 'row')
 	corMatrix <- appendSideToMatrixNames(corMatrix, 'S', 'col')
@@ -27,6 +34,7 @@ if (!is.null(rowNames) && !is.null(colNames) && all(rowNames == colNames)) {
 	saveRDS(degrees, paste("degrees", fileName, sep="")
 
 } else {
+	write('else:', stderr())
 	cat(format(serializeJSON(list(returnCode = 1, message = "Row names and column names don't match"))))	
 	file.remove(fileName)
 }
