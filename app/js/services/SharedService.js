862c6e3296b7bf704c151c719ecf4c05684a7a2c
Alex A <Alex A>
2016-06-06 09:38:09 -0400

    Created the InitializationService which initializes the variables and functions that are shared across controllers. Also made a huge fix to the submatrix.R script since it was no longer obtaining the edges for the second neighbours properly.

--- app/js/services/SharedService.js
+++
@@ -1,8 +0,0 @@
-var myModule = angular.module("myApp");
-myModule.factory('SharedService', function($http, $timeout, Upload) {
-    var service = {};
-
-    service.selectedFile = {file: null};
-
-    return service;
-});
