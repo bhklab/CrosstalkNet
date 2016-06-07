551bb6239ec02e5524c102bc7e97cc88f96302f9
Alex A <Alex A>
2016-06-07 14:52:52 -0400

    Finished getAllPaths.
    -Added a downloadable table for all of the paths.

--- R_Scripts/pathExistHelpers.R
+++ R_Scripts/pathExistHelpers.R
@@ -5,22 +5,13 @@ findAllPaths <- function(source, target, corMatrix) {
 	neighbours <- c()
 	paths <- createEmptyPaths()
 	result <- list(paths = NULL)
-	write('getGeneSuffix(source)', stderr())
-	write(getGeneSuffix(source), stderr())
-	write('getGeneSuffix(target)', stderr())
-	write(getGeneSuffix(target), stderr())
 
 	if (getGeneSuffix(source) == getGeneSuffix(target)) {
 		neighbours <- getNeighbours(corMatrix, source, c())
 
 		for (i in 1:length(neighbours)) {
 			secondLevelNeighbours <- getNeighbours(corMatrix, names(neighbours[i]), c())
-			write(' neighbours[i]', stderr())
-			write(neighbours[i], stderr())
-			write('secondLevelNeighbours', stderr())
-			write(secondLevelNeighbours, stderr())
 			if (target %in% names(secondLevelNeighbours)) {
-
 				numRows <- nrow(paths) 
 				paths[numRows + 1, "firstEdge"] = neighbours[i]
 				paths[numRows + 1, "intermediateNode"] = names(neighbours[i])
