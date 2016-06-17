9a6ff508a826db3735d5b763e5ee1fb86747fbd4
Alex A <Alex A>
2016-06-17 12:42:55 -0400

    Added basic jwt middleware.

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngMaterial',
+  'ngCookies',
   'ngFileUpload',
   'md.data.table',
   'myApp.controllers',
