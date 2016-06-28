0673053b7be70e2d9bc1785debe196e467f8073e
Alex A <Alex A>
2016-06-28 16:21:33 -0400

    Almost done restructuring everything.
    -There are now 3 different controllers for the different query tabs that
    exist.
    
    -The controllers that apply to the entire tab and not just the sidebar are
    there mainly just to enable the binding of values to controls. They don't
    drive the data but rather provide the html access to it.
    
    -Made all of the methods used in the html markup not require a "vm"
    argument. It is silly to be passing this argument every single time.

--- app/js/controllers/PathExistenceController.js
+++ app/js/controllers/PathExistenceController.js
@@ -3,68 +3,20 @@
 angular.module('myApp.controllers').controller('PathExistenceController', [
     '$scope',
     '$rootScope', 'RESTService',
-    'GraphConfigService', 'GlobalControls', 'InitializationService', 'ValidationService', 'SharedService', '$q', '$timeout',
-    function($scope, $rootScope, RESTService, GraphConfigService, GlobalControls, InitializationService, ValidationService, SharedService,
+    'GraphConfigService', 'GlobalControls', 'InitializationService', 'ValidationService', 'SharedService', 'ExportService', '$q', '$timeout',
+    function($scope, $rootScope, RESTService, GraphConfigService, GlobalControls, InitializationService, ValidationService, SharedService, ExportService,
         $q, $timeout) {
         var vm = this;
         vm.scope = $scope;
 
-        vm.ctrl = "pathExistence";
-        vm.graphType = "nonDelta";
-
-        //InitializationService.initializeCommonVariables(vm);
-
-        vm.genesOfInterest = [];
-        vm.pathExplorerSource = null;
-        vm.pathExplorerTarget = null;
-
-        vm.pathExplorerTextSource = null;
-        vm.pathExplorerTextTarget = null;
-
-        vm.allPaths = null;
+        vm.initialize = function(ctrl, type) {
+            vm.ctrl = ctrl;
+            vm.graphType = type;
+            vm.sdWithinTab = SharedService.data[vm.ctrl];
+            initializeVariables();
+        };
 
         vm.sharedData = SharedService.data.nonDelta;
-
-        vm.setPathExplorerGene = function(gene, which) {
-            if (gene != null) {
-                if (which == 'source') {
-                    vm.pathExplorerSource = gene;
-                } else {
-                    vm.pathExplorerTarget = gene;
-                }
-            }
-        };
-
-        vm.getAllPaths = function() {
-            if (vm.pathExplorerTarget == null || vm.pathExplorerSource == null) {
-                alert("Please select a source and target gene.");
-                return;
-            }
-
-            $rootScope.state = $rootScope.states.gettingAllPaths;
-            vm.allPaths = null;
-            vm.pathTarget = vm.pathExplorerTarget.value;
-            vm.pathSource = vm.pathExplorerSource.value;
-            RESTService.post('get-all-paths', {
-                target: vm.pathExplorerTarget.value,
-                source: vm.pathExplorerSource.value,
-                fileName: vm.sharedData.correlationFileActual
-            }).then(function(data) {
-                console.log(data);
-                vm.allPaths = data.paths;
-                $rootScope.state = $rootScope.states.finishedGettingAllPaths;
-                vm.display = "Tables";
-                vm.switchModel = true;
-            });
-        };
-
-        $rootScope.$watch(function() {
-            return vm.sharedData.correlationFileActual;
-        }, function() {
-            vm.genesOfInterest = [];
-            //vm.resetInputFields();
-            vm.neighbours = [];
-            vm.allPaths = null;
-        });
+        vm.exportTableToCSV = ExportService.exportTableToCSV;
     }
 ]);
---
+++ app/js/controllers/sidebar/PEQueryController.js
@@ -0,0 +1,70 @@
+'use strict';
+
+angular.module('myApp.controllers').controller('PEQueryController', [
+    '$scope',
+    '$rootScope', 'RESTService',
+    'GraphConfigService', 'GlobalControls', 'InitializationService', 'ValidationService', 'SharedService', 'QueryService', 'PathExistenceControls', '$q', '$timeout',
+    function($scope, $rootScope, RESTService, GraphConfigService, GlobalControls, InitializationService, ValidationService, SharedService, QueryService, PathExistenceControls,
+        $q, $timeout) {
+        var vm = this;
+        vm.scope = $scope;
+
+        vm.initialize = function(ctrl, type) {
+            vm.ctrl = ctrl;
+            vm.graphType = type;
+            vm.sdWithinTab = SharedService.data[vm.ctrl];
+            initializeVariables();
+        };
+
+        function initializeVariables() {
+            vm.pathExplorerSource = null;
+            vm.pathExplorerTarget = null;
+
+            vm.pathExplorerTextSource = null;
+            vm.pathExplorerTextTarget = null;
+
+            vm.sdWithinTab.allPaths = null;
+        }
+
+        vm.sharedData = SharedService.data.nonDelta;
+        PathExistenceControls.setMethods(vm);
+        GlobalControls.setMethodsSideBar(vm);
+
+        vm.setPathExplorerGene = function(gene, which) {
+            if (gene != null) {
+                if (which == 'source') {
+                    vm.pathExplorerSource = gene;
+                } else {
+                    vm.pathExplorerTarget = gene;
+                }
+            }
+        };
+
+        vm.refreshPaths = function() {
+            if (vm.pathExplorerTarget == null || vm.pathExplorerSource == null) {
+                alert("Please select a source and target gene.");
+                return;
+            }
+
+            SharedService.resetWTM(vm);
+            vm.sdWithinTab.pathTargetCached = vm.pathExplorerTarget.value;
+            vm.sdWithinTab.pathSourceCached = vm.pathExplorerSource.value;
+            QueryService.getAllPaths(vm).then(function(result) {
+                if (result.allPaths == null) {
+                    return;
+                }
+
+                vm.sdWithinTab.allPaths = data.allPaths;
+                $rootScope.state = $rootScope.states.finishedGettingAllPaths;
+            });
+        }
+
+        $scope.$watch(function() {
+            return vm.sharedData.correlationFileActual;
+        }, function(newValue, oldValue) {
+            if (newValue != "" && newValue != null && newValue != oldValue) {
+                initializeVariables();
+            }
+        });
+    }
+]);
