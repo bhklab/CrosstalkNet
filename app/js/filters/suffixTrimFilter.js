a4460b1f0d1f3f4073416ace8cfc7380beed8b24
Alex A <Alex A>
2016-06-30 14:35:35 -0400

    Changed png quality to be higher

--- app/js/filters/suffixTrimFilter.js
+++ app/js/filters/suffixTrimFilter.js
@@ -1,6 +1,11 @@
 //var filters = angular.module("myApp");
 angular.module('myApp.filters').filter('suffixTrim', function() {
-  return function(input) {
-    return input.slice(0, -2);
-  };
+    return function(input) {
+        if (input != null) {
+            return input.slice(0, -2);
+
+        }
+
+        return "";
+    };
 });
