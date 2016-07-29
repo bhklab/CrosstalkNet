4f55698dbbc9320b148446acfb5a6f0c33fe965b
Alex A <Alex A>
2016-07-29 11:37:39 -0400

    Fixed the degree explorer lag issue.
    -Made it so that the filtering of the degree explorer genes is done on the
    controller as opposed to the ng-repeat directive. This allows the use of a
    small array over which to iterate using ng-repeat. The size of this array
    is based on the limit for pagination. Instead of watching an array with
    20,000 elements, angularJS watches an array with 50 elements thus speeding
    up the app immensely.

--- app/js/controllers/degreeExplorerTab/DegreeExplorerController.js
+++ app/js/controllers/degreeExplorerTab/DegreeExplorerController.js
@@ -21,6 +21,11 @@
         vm.initializeController = initializeController;
         vm.goToGeneCard = goToGeneCard;
         vm.exportTopGenesToCSV = ExportService.exportTopGenesToCSV;
+
+        vm.search = search;
+        vm.paginateEpi = paginateEpi;
+        vm.paginateStroma = paginateStroma;
+
         /**
          * @summary Assigns the ctrl property of the controller and sets the appropriate within 
          * tab model based on the ctrl property.
@@ -34,6 +39,93 @@
         }
 
         /**
+         * @summary Filters the the genes from the specified tissue type based on the search 
+         * query made by the user.
+         *
+         * @param {String} tissue A string that is either epi or stroma indicating which set
+         * of genes is to be filtered.
+         *
+         */
+        function search(tissue) {
+            var filtered = [];
+            var page = 1;
+            var limit = vm.sdWithinTab.pagination[tissue].query.limit;
+
+            vm.sdWithinTab.pagination[tissue].query.page = page;
+            filtered = filterGenesByName(vm.sdWithinTab.topGenes[tissue], vm.sdWithinTab.search[tissue]);
+            vm.sdWithinTab.filtered[tissue].total = filtered.length;
+            // Add code for length here
+
+            vm.sdWithinTab.filtered[tissue].genes = filterGenesPagination(filtered, page, limit);
+        }
+
+        /**
+         * @summary Refreshes the displayed list of epi genes based on the new page that was navigated
+         * to.
+         * 
+         * @param {Number} page The new page number.
+         * @param {Number} limit The number of genes to limit the result to.
+         */
+        function paginateEpi(page, limit) {
+            var filtered = [];
+
+            filtered = filterGenesByName(vm.sdWithinTab.topGenes.epi, vm.sdWithinTab.search.epi);
+            vm.sdWithinTab.filtered.epi.genes = filterGenesPagination(filtered, page, limit);
+        }
+
+        /**
+         * @summary Refreshes the displayed list of stroma genes based on the new page that was navigated
+         * to.
+         * 
+         * @param {Number} page The new page number.
+         * @param {Number} limit The number of genes to limit the result to.
+         */
+        function paginateStroma(page, limit) {
+            var filtered = [];
+
+            filtered = filterGenesByName(vm.sdWithinTab.topGenes.stroma, vm.sdWithinTab.search.stroma);
+            vm.sdWithinTab.filtered.stroma.genes = filterGenesPagination(filtered, page, limit);
+        }
+
+        /**
+         * @summary Filters the specified array of genes by a given gene name.
+         *
+         *
+         * @param {Array} genes An array of gene objects.
+         * @param {String} name The gene name to filter the array of genes by.
+         * @return {Array} An array of gene objects whose properties match the
+         * name specified.
+         */
+        function filterGenesByName(genes, name) {
+            var filtered = [];
+
+            filtered = $filter('filter')(genes, name);
+
+            return filtered;
+        }
+
+        /** 
+         * @summary Filters the specified array of genes to limit them
+         * based on the page number that the user is currently on.
+         *
+         * @param {Array} genes An array of gene objects.
+         * @param {Number} page The page that the user is currently on. This
+         * is used in combination with the limit to determine which subset 
+         * of genes to return.
+         * @param {Number} limit The maximum number of genes to return.
+         * @return {Array} An array of gene objects that is a subset of 
+         * the genes specified limited to the genes for a certain page 
+         * number and limit.
+         */ 
+        function filterGenesPagination(genes, page, limit) {
+            var filtered = [];
+
+            filtered = $filter('limitTo')(genes, limit, (page - 1) * limit)
+
+            return filtered;
+        }
+
+        /**
          * @summary Opens the gene card for the specified gene in another tab.
          *
          * @param {Object} gene The gene to open the gene card for.
@@ -41,5 +133,12 @@
         function goToGeneCard(gene) {
             $window.open(GlobalSharedData.geneCardURL + $filter('suffixTrim')(gene.value));
         }
+
+        $scope.$watch('vm.sdWithinTab.dataLoaded', function(newValue, oldValue) {
+            if (newValue != oldValue && oldValue == false) {
+                search('epi');
+                search('stroma');
+            }
+        })
     }
 })();
