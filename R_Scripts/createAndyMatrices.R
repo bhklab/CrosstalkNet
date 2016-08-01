fc54366e8d886545c1c7b024d724af0d9eb86efa
Alex A <Alex A>
2016-08-01 11:32:21 -0400

    Added state for loading top genes.
    -Also moved some of the functions from the controls services to their
    respective controllers in order to avoid confusion.

--- R_Scripts/createAndyMatrices.R
+++ R_Scripts/createAndyMatrices.R
@@ -2,44 +2,45 @@ library(psych)
 library(methods)
 library(Matrix)
 
-tempMat <- matrix(1:25, nrow=5, ncol=5)
-
-tempMat <- as(tempMat, "sparseMatrix")
-
 setwd('/mnt/work1/users/home2/aadam/R_Scripts')
 
 load('Andy-ScaledDataSets.RData')
 
 ProbeGeneMap <- read.csv("028004_D_AA_20140813.txt",stringsAsFactors = F, sep = "\t")
 
-CorTES.Normal <- cor(t(allmat.by.ctype.by.ES$No$Epi),t(allmat.by.ctype.by.ES$No$Str))
-CorTES.BrP <- cor(t(allmat.by.ctype.by.ES$BrP$Epi),t(allmat.by.ctype.by.ES$BrP$Str))
-CorTES.BrN <- cor(t(allmat.by.ctype.by.ES$BrN$Epi),t(allmat.by.ctype.by.ES$BrN$Str))
+CorMatrix.Normal <- cor(t(allmat.by.ctype.by.ES$No$Epi),t(allmat.by.ctype.by.ES$No$Str))
+CorMatrix.BrP <- cor(t(allmat.by.ctype.by.ES$BrP$Epi),t(allmat.by.ctype.by.ES$BrP$Str))
+CorMatrix.BrN <- cor(t(allmat.by.ctype.by.ES$BrN$Epi),t(allmat.by.ctype.by.ES$BrN$Str))
 
-# rownames(CorTES.Normal) = ProbeGeneMap$GeneSymbol[match(rownames(CorTES.Normal), (ProbeGeneMap$EntrezGeneID))]
-# colnames(CorTES.Normal) = ProbeGeneMap$GeneSymbol[match(colnames(CorTES.Normal), (ProbeGeneMap$EntrezGeneID))]
+CorMatrix.Delta.Positive <- CorMatrix.BrP - CorMatrix.Normal
+CorMatrix.Delta.Negative <- CorMatrix.BrN - CorMatrix.Normal
 
-# rownames(CorTES.BrP) = ProbeGeneMap$GeneSymbol[match(rownames(CorTES.BrP), (ProbeGeneMap$EntrezGeneID))]
-# colnames(CorTES.BrP) = ProbeGeneMap$GeneSymbol[match(colnames(CorTES.BrP), (ProbeGeneMap$EntrezGeneID))]
+Signif.Normal <- r.test(22, CorMatrix.Normal)
+Signif.BrP <- r.test(54, CorMatrix.BrP)
+Signif.BrN <- r.test(28, CorMatrix.BrN)
 
-# rownames(CorTES.BrN) = ProbeGeneMap$GeneSymbol[match(rownames(CorTES.BrN), (ProbeGeneMap$EntrezGeneID))]
-# colnames(CorTES.BrN) = ProbeGeneMap$GeneSymbol[match(colnames(CorTES.BrN), (ProbeGeneMap$EntrezGeneID))]
-
-Signif.Normal <- r.test(22, CorTES.Normal)
-Signif.BrP <- r.test(54, CorTES.BrP)
-Signif.BrN <- r.test(28, CorTES.BrN)
+Signif.Delta.Positive <- r.test(22, CorMatrix.Normal, CorMatrix.BrP, n2=54)
+Signif.Delta.Negative <- r.test(22, CorMatrix.Normal, CorMatrix.BrN, n2=28)
 
 ptm <- proc.time()
 
-TESNormal.padj.FDR <- p.adjust(Signif.Normal$p,method="fdr",length(Signif.Normal$p)) # FDR Adjustment for multiple testing
-TESBrP.padj.FDR <- p.adjust(Signif.BrP$p,method="fdr",length(Signif.BrP$p)) # FDR Adjustment for multiple testing
-TESBrN.padj.FDR <- p.adjust(Signif.BrN$p,method="fdr",length(Signif.BrN$p)) # FDR Adjustment for multiple testing
+NormalES.padj.FDR <- p.adjust(Signif.Normal$p,method="fdr",length(Signif.Normal$p)) # FDR Adjustment for multiple testing
+BrPES.padj.FDR <- p.adjust(Signif.BrP$p,method="fdr",length(Signif.BrP$p)) # FDR Adjustment for multiple testing
+BrNES.padj.FDR <- p.adjust(Signif.BrN$p,method="fdr",length(Signif.BrN$p)) # FDR Adjustment for multiple testing
 
-CorTES.Normal.FDRadj.05 <- CorTES.Normal 
+Delta.Positive.padj.FDR <- p.adjust(Signif.Delta.Positive$p,method="fdr",length(Signif.Delta.Positive$p)) # FDR Adjustment for multiple testing
+Delta.Negative.padj.FDR <- p.adjust(Signif.Delta.Negative$p,method="fdr",length(Signif.Delta.Negative$p)) # FDR Adjustment for multiple testing
 
-CorTES.BrP.FDRadj.05 <- CorTES.BrP 
+CorMatrix.Normal.FDRadj.05 <- CorMatrix.Normal 
 
-CorTES.BrN.FDRadj.05 <- CorTES.BrN 
+CorMatrix.BrP.FDRadj.05 <- CorMatrix.BrP 
+
+CorMatrix.BrN.FDRadj.05 <- CorMatrix.BrN 
+
+CorMatrix.Delta.Positive.FDRadj.05 <- CorMatrix.Delta.Positive
+
+CorMatrix.Delta.Negative.FDRadj.05 <- CorMatrix.Delta.Negative
+
 
 timeDif <- proc.time() - ptm 
 write("Significance Test Took: ", stderr())
@@ -47,14 +48,20 @@ write(timeDif, stderr())
 
 ptm <- proc.time()
 
-CorTES.Normal.FDRadj.05[TESNormal.padj.FDR > 0.05] <- 0
-CorTES.Normal.FDRadj.05 <- as(CorTES.Normal.FDRadj.05, "sparseMatrix")
+CorMatrix.Normal.FDRadj.05[NormalES.padj.FDR > 0.05] <- 0
+CorMatrix.Normal.FDRadj.05 <- as(CorMatrix.Normal.FDRadj.05, "sparseMatrix")
 
-CorTES.BrP.FDRadj.05[TESBrP.padj.FDR > 0.05] <- 0
-CorTES.BrP.FDRadj.05 <- as(CorTES.BrP.FDRadj.05, "sparseMatrix")
+CorMatrix.BrP.FDRadj.05[BrPES.padj.FDR > 0.05] <- 0
+CorMatrix.BrP.FDRadj.05 <- as(CorMatrix.BrP.FDRadj.05, "sparseMatrix")
 
-CorTES.BrN.FDRadj.05[TESBrN.padj.FDR > 0.05] <- 0
-CorTES.BrN.FDRadj.05 <- as(CorTES.BrN.FDRadj.05, "sparseMatrix")
+CorMatrix.BrN.FDRadj.05[BrNES.padj.FDR > 0.05] <- 0
+CorMatrix.BrN.FDRadj.05 <- as(CorMatrix.BrN.FDRadj.05, "sparseMatrix")
+
+CorMatrix.Delta.Positive.FDRadj.05[Delta.Positive.padj.FDR > 0.05] <- 0
+CorMatrix.Delta.Positive.FDRadj.05 <- as(CorMatrix.Delta.Positive.FDRadj.05, "sparseMatrix")
+
+CorMatrix.Delta.Negative.FDRadj.05[Delta.Negative.padj.FDR > 0.05] <- 0
+CorMatrix.Delta.Negative.FDRadj.05 <- as(CorMatrix.Delta.Negative.FDRadj.05, "sparseMatrix")
 
 
 timeDif <- proc.time() - ptm 
@@ -63,8 +70,12 @@ write(timeDif, stderr())
 
 ptm <- proc.time()
 
-saveRDS(CorTES.Normal.FDRadj.05, file='normal.05.RData')
+saveRDS(CorMatrix.Normal.FDRadj.05, file='normal.correlation.05.RData')
 
-saveRDS(CorTES.BrP.FDRadj.05, file='breastPositive.05.RData')
+saveRDS(CorMatrix.BrP.FDRadj.05, file='breastPositive.correlation.05.RData')
 
-saveRDS(CorTES.BrN.FDRadj.05, file='breastNegative.05.RData')
+saveRDS(CorMatrix.BrN.FDRadj.05, file='breastNegative.correlation.05.RData')
+
+saveRDS(CorMatrix.Delta.Positive.FDRadj.05, file='deltaPositive.correlation.05.RData')
+
+saveRDS(CorMatrix.Delta.Negative.FDRadj.05, file='deltaNegative.correlation.05.RData')
